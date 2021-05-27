5bce1e64bcce7cc08337968a949f632801ff8b2f
Tristan Hull <tristan@kenpokicks.com>
2021-05-27 16:03:13 -0700

    links finished

--- src/app/pages/links/links.component.ts
+++ src/app/pages/links/links.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { link } from '../../shared/models/link.model'
 
 @Component({
   selector: 'app-links',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinksComponent implements OnInit {
 
-  constructor() { }
+  public links: link[] = [
+    {
+    content: "This site is a great place to visit if you are interested in the thoughts of a true warrior/philosopher.",
+    href: "http://www.molonlabe.net/Commentaries/",
+    href_name: "Jeff Cooper's Commentaries"
+    },
+    {
+      content: "If you are looking for an immigration attorney, look no further than at Aime Katambwe, founder of",
+      href: "http://worldesquire.com/",
+      href_name: "WorldEsquire Law Firm"
+    },
+    {
+      content: "If you require firearms instruction, or would like to take the necessary training to get your Nevada and/or Utah Concealed Weapons Permit, I very highly recommend Rick Cross with",
+      href: "http://www.besafefirearms.com/",
+      href_name: "Be Safe Firearms Instruction"
+    },
+  ]
+
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/shared/models/link.model.ts
@@ -0,0 +1,5 @@
+export class link {
+    public content: String;
+    public href: String;
+    public href_name: String;
+}
